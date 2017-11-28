b1be3e138afadd51b7658f53faa8967785c9d211
Andrei Zhuk1 <Andrei_Zhuk1@epam.com>
2017-11-28 16:38:55 +0300

    es2015-task/fix: fix implementation of articles displaying

--- es2015-task/app/assets/scripts/modules/Article.js
+++ es2015-task/app/assets/scripts/modules/Article.js
@@ -4,65 +4,37 @@ export default class Article {
     }
 
     makeImage() {
-        let articleImageLink = document.createElement('a');
-        articleImageLink.href = this.data.url;
-        articleImageLink.classList.add("articleImageLink")
-        let articleImage = document.createElement('img');
-        articleImage.src = this.data.urlToImage;
-        articleImage.classList.add("articleImage")
-        articleImageLink.appendChild(articleImage);
-        return articleImageLink
+        return `<a href="${this.data.url}" class="articleImageLink">
+                    <img src="${this.data.urlToImage}" class="articleImage">
+                </a>`
     }
 
     makeTitle() {
-        let articleTitle = document.createElement('a');
-        articleTitle.href = this.data.url;
-        articleTitle.textContent = this.data.title;
-        articleTitle.classList.add("articleTitle");
-        return articleTitle
+        return `<a href="${this.data.url}" class="articleTitle">${this.data.title}</a>`
     }
 
     makeAuthor() {
-        let articleAuthor = document.createElement('span');
-        articleAuthor.innerText = this.data.author ? `Author: ${this.data.author}` : '';
-        articleAuthor.classList.add("articleAuthor");
-        return articleAuthor;
+        return `<span class="articleAuthor">${this.data.author ? `Author: ${this.data.author}` : ''}</span>`
     }
 
     makePublishedTime() {
-        let articleTime = document.createElement('span');
         let time = new Date(this.data.publishedAt);
-        articleTime.innerText = `Published at ${time.getHours() < 10 ? '0' + time.getHours() : time.getHours()}:${time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()} ${time.getDate() < 10 ? '0' + time.getDate() : time.getDate()}.${time.getMonth() + 1 < 10 ? '0' + time.getMonth() + 1 : time.getMonth() + 1}.${time.getFullYear()}`;
-        articleTime.classList.add("articleTime");
-        return articleTime
+        return `<span class="articleTime">
+                    Published at ${time.getHours() < 10 ? '0' + time.getHours() : time.getHours()}:${time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()} ${time.getDate() < 10 ? '0' + time.getDate() : time.getDate()}.${time.getMonth() + 1 < 10 ? '0' + time.getMonth() + 1 : time.getMonth() + 1}.${time.getFullYear()}
+                </span>`
     }
 
     makeDescription() {
-        let articleDescription = document.createElement('p');
-        articleDescription.innerText = this.data.description;
-        articleDescription.classList.add("articleDescription");
-        return articleDescription;
+        return `<p class="articleDescription">${this.data.description}</p>`
     }
 
     makeContentContainer() {
-        let articleContent = document.createElement('div');
-        articleContent.classList.add("articleContent");
-
-        articleContent.appendChild(this.makeTitle())
-        articleContent.appendChild(this.makeAuthor())
-        articleContent.appendChild(this.makePublishedTime())
-        articleContent.appendChild(this.makeDescription())
-
-        return articleContent;
+        return `<div class="articleContent">
+                    ${this.makeTitle()} ${this.makeAuthor()} ${this.makePublishedTime()} ${this.makeDescription()}
+                </div>`
     }
 
     makeArticle() {
-        let article = document.createElement('li');
-        article.classList.add("article");
-
-        article.appendChild(this.makeImage());
-        article.appendChild(this.makeContentContainer());
-
-        return article;
+        return `<li class="article">${this.makeImage()} ${this.makeContentContainer()}</li>`
     }
 }
--- es2015-task/app/assets/scripts/modules/displayArticles.js
+++ es2015-task/app/assets/scripts/modules/displayArticles.js
@@ -3,23 +3,17 @@ import Article from "./Article";
 
 export default function displayArticles(articles) {
     let container = document.getElementById('articles-container');
-    let articlesTitle = document.createElement('h2');
-    articlesTitle.innerText = "Articles:";
-    articlesTitle.classList.add("articlesTitle")
 
     if (container.children.length !== 0) {
         container.removeChild(container.children[0]);
         container.removeChild(container.children[1])
     }
-
-    let articleList = document.createElement('ul');
-    articleList.classList.add("articleList")
+    let articlesString = "";
 
     for (let i = 0; i < articles.content.length; i++) {
         let article = new Article(articles.content[i]);
-        articleList.appendChild(article.makeArticle())
+        articlesString += article.makeArticle();
     }
-
-    container.appendChild(articlesTitle);
-    container.appendChild(articleList);
+    container.innerHTML = `<h2 class="articlesTitle">Articles:</h2>
+                           <ul class="articleList">${articlesString}</ul>`
 }
