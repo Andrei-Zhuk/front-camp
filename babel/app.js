5e94ff20d6bc53a564f2db3b506084f22f45293f
Andrei-Zhuk <zhuk_andrey@tut.by>
2018-02-20 00:01:50 +0300

    node-2: finish authorisation functionallity

--- babel/app.js
+++ babel/app.js
@@ -1,55 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const uniqid = require('uniqid');
 const winston = require('winston');
 const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
+const connect = require('connect-ensure-login');
 mongoose.connect('mongodb://localhost/test');
 
-passport.use(new Strategy(
-    function(username, password, cb) {
-        User.findOne({email: username, password: password}, function (err, user) {
-          if (err) return cb(err)
-          if (!user) { return cb(null, false); }
-          return cb(null, user);
-        })
-    }
-));
+passport.use(new Strategy((username, password, cb) => {
+    User.findOne({username, password}, (err, user) => {
+        if (err) return cb(err)
+        if (!user) { return cb(null, false); }
+        return cb(null, user);
+    })
+}));
 
-passport.serializeUser(function(user, cb) {
-    console.log('serialized');
+passport.serializeUser((user, cb) => {
     cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
-    console.log('deserialized');
-    User.findOne({id: id}, function (err, user) {
+passport.deserializeUser((id, cb) => {
+    User.findOne({_id: id}, (err, user) => {
         if (err) { return cb(err); }
         cb(null, user);
     });
 });
 
-function checkLog (req, res, next) {
-    console.log(req.user);
-    if (req.user) {
-        next()
-    } else {
-        res.redirect('/login')
-    }
-}
-
 const app = express();
-const blogs = [];
+
 const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: 'combined.log' })
   ]
 });
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', (console.error.bind(console, 'connection error:')));
-db.once('open', function() {
+db.once('open', () => {
   console.log('connected to db');
 });
 
@@ -58,7 +45,7 @@ const blogSchema = mongoose.Schema({
     title: String
 })
 const userSchema = mongoose.Schema({
-    email: String,
+    username: String,
     password: String
 })
 
@@ -68,66 +55,78 @@ const User = mongoose.model('User', userSchema)
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+    logger.log({
+        level: "info",
+        url: req.url,
+        date: new Date()
+    });
+    next();
+})
+
 app.get('/login', (req, res) => {
     res.render('login');
 })
 
-app.post('/login', passport.authenticate('local'), function (req, res) {
-    console.log("post user", req.user);
+app.post('/login', passport.authenticate('local'), (req, res) => {
     res.redirect('/')
 })
 
-app.get('/users', checkLog, (req, res) => {
-  logger.log({
-      level: "info",
-      url: req.url,
-      date: new Date()
-  });
-  User.find(function (err, blogs) {
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.post('/register', (req, res, next) => {
+  if (req.body.password1 !== req.body.password2) {
+    res.redirect('/register')
+  } else {
+    User.findOne({username: req.body.username}, (err, user) => {
+      if (user) {
+        res.redirect('/register')
+      } else {
+        new User({username: req.body.username, password: req.body.password1}).save((err, user) => {
+          res.redirect('/login')
+        })
+      }
+    })
+  }
+})
+
+app.get('/users', connect.ensureLoggedIn(), (req, res) => {
+  User.find((err, blogs) => {
     if (err) return next(err)
     res.json(blogs)
   })
 });
 
-app.get('/blogs', (req, res) => {
-  logger.log({
-      level: "info",
-      url: req.url,
-      date: new Date()
-  });
-  Blogs.find(function (err, blogs) {
+app.get('/blogs',(req, res) => {
+  Blogs.find((err, blogs) => {
     if (err) return next(err)
     res.json(blogs)
   })
 });
 
 app.get('/blogs/:id', (req, res) => {
-  logger.log({
-      level: "info",
-      url: req.url,
-      date: new Date()
-  });
-  Blogs.findOne({_id: req.params.id},function (err, blogs) {
+  Blogs.findOne({_id: req.params.id}, (err, blogs) => {
     if (err) return next(err)
     res.json(blogs)
   })
 });
 
-app.post('/blogs', (req, res) => {
-  logger.log({
-      level: "info",
-      url: req.url,
-      date: new Date()
-  });
-  let blog = new Blogs({title: req.body.title, _id: req.body.id});
+app.post('/blogs', (req, res, next) => {
+  let blog = new Blogs({_id: req.body.id, title: req.body.title});
   blog.save((err, blog) => {
       if (err) return next(err)
       res.json(blog);
@@ -135,11 +134,6 @@ app.post('/blogs', (req, res) => {
 })
 
 app.put('/blogs/:id', (req, res) => {
-  logger.log({
-      level: "info",
-      url: req.url,
-      date: new Date()
-  });
   Blogs.findByIdAndUpdate(req.params.id, {title: req.body.title}, (err, blog) => {
       if (err) return next(err)
       res.json(blog)
@@ -147,11 +141,6 @@ app.put('/blogs/:id', (req, res) => {
 })
 
 app.delete('/blogs/:id', (req, res, next) => {
-  logger.log({
-      level: "info",
-      url: req.url,
-      date: new Date()
-  });
   Blogs.findOne({_id: req.params.id}, (err, blog) => {
       if (err) return next(err)
       if (!blog) return next(new Error())
@@ -162,19 +151,14 @@ app.delete('/blogs/:id', (req, res, next) => {
   })
 })
 
-app.get('/*', function (req, res) {
-  console.log("get user", req.user);
-  logger.log({
-      level: "info",
-      url: req.url,
-      date: new Date()
-  });
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+app.get('/*', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!', user: req.user });
 });
 
 app.use((err, req, res, next) => {
+    err.stack = null;
     console.error(err);
-    res.status(500).send(err)
+    res.status(500).send({err: err.stack})
 })
 
 app.listen(3000, () => {
